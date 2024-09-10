9ce7e5bdd0928278b3e55902b7f4a009cd945ebe
Maria Amasialidou <79280783+MariaAma@users.noreply.github.com>
2024-09-10 20:42:56 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import appFile
+
+st.set_page_config(page_title="anime_recommendations", page_icon="🚀")
+
+st.header(':blue[**Welcome to this project!**]')
+st.subheader(":violet[*You don't know, what anime to watch?*]")
+st.subheader(":violet[DON'T WORRY, I got you!]")
+
+title = st.text_input(":blue[Tell me your favorite Anime.]")
+
+if st.button(":blue[Sumbit]"):
+    appFile.app(title)
+    
 
